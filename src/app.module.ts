187c1099598955bf937fbc6531d76fc81850a563
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2026-02-15 18:10:52 -0500

    Initial project structure

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'museum',
+    entities: [],
+    dropSchema: true,
+    synchronize: true,
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
