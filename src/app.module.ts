40da1dd7bec000297c5a1ffd91067738a0a8f5bd
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2026-03-18 22:04:19 -0500

    fix initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,30 +4,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD, typeof process.env.DB_PASSWORD);
-console.log('DB_USERNAME:', process.env.DB_USERNAME);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_NAME:', process.env.DB_NAME);
 
 @Module({
   imports: [
-<<<<<<< HEAD
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT as string),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-=======
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -42,7 +21,6 @@ console.log('DB_NAME:', process.env.DB_NAME);
         autoLoadEntities: true,
         synchronize: true, // solo dev
       }),
->>>>>>> be6395da3b428800f157f7880ddbff6dfc4348fb
     }),
   ],
   controllers: [AppController],
